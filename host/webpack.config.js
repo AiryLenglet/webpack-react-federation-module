93828c521028a75459f2724064410921136e29f7
Airy LENGLET <airy+github@lenglet.me>
2023-05-29 09:41:57 +0200

    add hash in production to evict cache

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -1,33 +1,39 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "build"),
-    },
-    devServer: {
-        static: {
-          directory: path.join(__dirname, "build"),
+
+module.exports = (env, argv) => {
+
+    const filename = argv.mode === 'production' ? '[name].[hash].js' : 'main.js';
+
+    return {
+        entry: "./src/index.js",
+        output: {
+            filename: filename,
+            path: path.resolve(__dirname, "build"),
         },
-        port: 3000,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ["babel-loader"],
-            }
+        devServer: {
+            static: {
+              directory: path.join(__dirname, "build"),
+            },
+            port: 3000,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                }
+            ],
+        },
+        resolve: {
+            extensions: ["*", ".js", ".jsx"],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(__dirname, "public", "index.html"),
+            }),
         ],
-    },
-    resolve: {
-        extensions: ["*", ".js", ".jsx"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
-        }),
-    ],
+    };
 };
